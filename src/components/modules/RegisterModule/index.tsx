d1a0b943a94b413d54fb0ed965561535a689204f
Muhammad Daffa'I Rafi Prasetyo <daffarafip24@gmail.com>
2023-08-17 16:12:39 +0700

    Register Menu

--- src/components/modules/RegisterModule/index.tsx
+++ src/components/modules/RegisterModule/index.tsx
@@ -1,3 +1,5 @@
+import { GlowingSpan } from '@elements'
+import Link from 'next/link'
 import React from 'react'
 // import {HeroSection, FAQSection} from './sections
 // import {} from './module-elements'
@@ -5,5 +7,31 @@ import React from 'react'
 export const RegisterModule: React.FC = () => {
   // TODO: Write module's logic
 
-  return <>Ini Register</>
+  return (
+    <section className="flex flex-col gap-10 justify-center items-center min-h-screen">
+      <div className="grid grid-cols-2 gap-10">
+        <Link
+          href={'/register/bcc'}
+          className="flex flex-col items-center group"
+        >
+          <span className="text-9xl -mb-8 font-Sarmandy tracking-[0.2em] font-extrabold">
+            <GlowingSpan color="blue">BCC</GlowingSpan>
+          </span>
+          <span className="text-[#c6e2de]">3 Members</span>
+        </Link>
+        <Link href={'/register/bridge'} className="flex flex-col items-center">
+          <span className="text-9xl -mb-8 font-Sarmandy tracking-[0.2em] font-extrabold">
+            <GlowingSpan color="blue">BRIDGE</GlowingSpan>
+          </span>
+          <span className="text-[#c6e2de]">3-4 Members </span>
+        </Link>
+      </div>
+      <Link href={'/register/cetc'} className="flex flex-col items-center">
+        <span className="text-9xl -mb-8 font-Sarmandy tracking-[0.2em] font-extrabold">
+          <GlowingSpan color="blue">CETC</GlowingSpan>
+        </span>
+        <span className="text-[#c6e2de]">2-3 Members </span>
+      </Link>
+    </section>
+  )
 }
