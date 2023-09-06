e95376a0a4c7f5b1d21dced579f14edbbf24500d
Muhammad Daffa'I Rafi Prasetyo <daffarafip24@gmail.com>
2023-09-06 12:39:09 +0700

    file input max size 3.5mb

--- src/components/modules/RegisterCompetitionModule/module-elements/FileInput.tsx
+++ src/components/modules/RegisterCompetitionModule/module-elements/FileInput.tsx
@@ -8,42 +8,42 @@ export const FileInput: React.FC<FileInputProps> = ({
   setValue,
 }) => {
   return (
-    <div className="flex gap-1 flex-col lg:flex-row w-full">
+    <div className="flex items-center gap-1 flex-col lg:flex-row w-full">
       <label htmlFor={id} className="w-2/6 whitespace-nowrap">
         {label}
       </label>
-      <input
-        id={id}
-        type={'file'}
-        accept="image/*"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          const files = e.target.files
-          if (!files) return
-          const file = files[0]
-
-          if (!file) {
-            alert('Mohon masukan file!')
-            e.target.value = ''
-            return
-          }
-
-          if (!file.type.startsWith('image/')) {
-            alert('File harus bertipe gambar.')
-            e.target.value = ''
-            return
-          }
-
-          if (file.size > 4 * 1024 * 1024) {
-            alert('File tidak boleh lebih dari 4 MB.')
-            e.target.value = ''
-            return
-          }
-          setValue(e)
-        }}
-        className="w-full"
-        name={name}
-        required
-      />
+      <div className="w-full flex flex-col">
+        <input
+          id={id}
+          type={'file'}
+          accept="image/*"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            const files = e.target.files
+            if (!files) return
+            const file = files[0]
+            if (!file) {
+              alert('Mohon masukan file!')
+              e.target.value = ''
+              return
+            }
+            if (!file.type.startsWith('image/')) {
+              alert('File harus bertipe gambar.')
+              e.target.value = ''
+              return
+            }
+            if (file.size > 3.5 * 1024 * 1024) {
+              alert('File tidak boleh lebih dari 3.5 MB.')
+              e.target.value = ''
+              return
+            }
+            setValue(e)
+          }}
+          className="w-fit"
+          name={name}
+          required
+        />
+        <span className="italic text-xs">(max size: 3.5mb)</span>
+      </div>
     </div>
   )
 }
