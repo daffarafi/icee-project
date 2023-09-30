5c8dc3b1adacebec98da85aec3d32ae525f64c29
Arxeno <rmfbkw04@gmail.com>
2023-09-30 11:53:21 +0700

    change registration form to gform

--- src/components/modules/RegisterModule/index.tsx
+++ src/components/modules/RegisterModule/index.tsx
@@ -3,21 +3,24 @@ import Link from 'next/link'
 import React from 'react'
 
 export const RegisterModule: React.FC = () => {
+  const gformLink =
+    'https://docs.google.com/forms/d/e/1FAIpQLSdo2LLV8ghvtoF64cgUHFLiM1aiMjNrGHmGjwJ5tJ6Yfl__4w/viewform'
+
   return (
     <section className="flex flex-col gap-10 justify-center items-center min-h-screen py-44">
-      <Link href={'/register/bridge'} className="flex flex-col items-center">
+      <Link href={gformLink} className="flex flex-col items-center">
         <span className="text-3xl sm:text-6xl md:text-7xl lg:text-8xl mb-2 font-Sarmandy tracking-[0.2em] font-extrabold">
           <GlowingSpan color="blue">BRIDGE</GlowingSpan>
         </span>
         <span className="text-[#c6e2de]">3 Members </span>
       </Link>
-      <Link href={'/register/bcc'} className="flex flex-col items-center group">
+      <Link href={gformLink} className="flex flex-col items-center group">
         <span className="text-3xl sm:text-6xl md:text-7xl lg:text-8xl mb-2 font-Sarmandy tracking-[0.2em] font-extrabold">
           <GlowingSpan color="blue">BCC</GlowingSpan>
         </span>
         <span className="text-[#c6e2de]">3-4 Members</span>
       </Link>
-      <Link href={'/register/tender'} className="flex flex-col items-center">
+      <Link href={gformLink} className="flex flex-col items-center">
         <span className="text-3xl sm:text-6xl md:text-7xl lg:text-8xl mb-2 font-Sarmandy tracking-[0.2em] font-extrabold">
           <GlowingSpan color="blue">TENDER</GlowingSpan>
         </span>
